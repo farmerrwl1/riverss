6eeb8120ecb2f70e0df280a221b500840dc5ac7a
farmerrwl1 <161303378+farmerrwl1@users.noreply.github.com>
2024-02-27 09:02:19 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('postgres', 'postgres', '123123', {
-  host: 'localhost',
+const sequelize = new Sequelize('rivers_443g', 'rivers_443g_user', 'YUqIjLJzwSB7XOlzYISR4DwIO4aBzYN9', {
+  host: 'dpg-cnef2ansc6pc73eg0scg-a',
   dialect: 'postgres',
-  port: 5432
+  port: 5432,
 });
 
 module.exports = sequelize;
