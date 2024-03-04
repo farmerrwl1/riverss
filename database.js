1433f01630f9eecff30dece7fa0e4c8873289e1a
farmerrwl1 <161303378+farmerrwl1@users.noreply.github.com>
2024-03-04 09:44:03 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('rivers_443g', 'rivers_443g_user', 'YUqIjLJzwSB7XOlzYISR4DwIO4aBzYN9', {
-  host: 'dpg-cnef2ansc6pc73eg0scg-a',
+const sequelize = new Sequelize('rivers_l5s4', 'rivers', 'tS7ysQmikJboudKy86ffJu0Ys0MugqTu', {
+  host: 'dpg-cnimp95jm4es738o2he0-a',
   dialect: 'postgres',
   port: 5432,
 });
